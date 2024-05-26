e11dbdec9c81dfb5b313af248cb6369eb972509f
tuna70803 <tuna70803@gmail.com>
2024-05-26 14:34:36 +0900

    feat: PWA 설정 추가
    PWA, 특히 아이폰 같은 모바일 폰에서 홈 아이콘을 등록할 수 있는 설정을
    추가한다. 우선 앱 아이콘과 스플래시 화면용 이미지들을 크기 별로 추가한다.
    그리고 이를 사용하는 메타 설정을 레이아웃 설정에 추가한다.
    홈 아이콘 등록을 위해서는 서비스워커 설정이 있어야하므로 특별한 처리는
    하지않는 워커 설정 파일을 하나 추가한다.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import type { Metadata } from "next";
 import Main from "./Main";
 
 const Page = () => {
   return <Main />;
-};
-
-export const metadata: Metadata = {
-  title: "thunderous crayfish bus",
-  description: "내 버스는 언제오나?",
 };
 
 export default Page;
