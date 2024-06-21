738da819d6a816e39d272f1fa3da2fbc866ad04c
tuna70803 <tuna70803@gmail.com>
2024-06-21 23:38:06 +0900

    fix: 서버와 클라이언트 렌더링 트리가 안맞는 문제 수정
    서비스를 실행하면 버스 정보를 표시하는 컴포넌트에서 서버와 클라이언트의
    렌더링 트리 내용이 안맞는 다는 경고가 발생한다. 아무래도 데이터 설정
    과정에서 렌더링 결과물의 차이가 발생하는 것 같다. 보통 이런 경우
    useState로 첫 렌더링은 서버와 클라이언트가 같은 값으로 렌더링 되게 만들고
    useEffect로 하이드레이션 이후 실제 적용할 값으로 바꾸는 방식을 사용하는 것
    같다. 로직상으론 불필요한 내용이지만 해당 문제를 수정하기위해 위 방식을
    적용한다.

--- components/widget/BusDepartureCard/BusDepartureCard.tsx
+++ components/widget/BusDepartureCard/BusDepartureCard.tsx
@@ -1,13 +1,14 @@
+import { useEffect, useState } from 'react';
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
 } from '@/components/ui/card';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import BusNumber from '../BusNumber';
 import RemainTimeLabel from './RemainTimeLabel';
 import EstimatedArrivalTimeLabel from './EstimatedArrivalTimeLabel';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 interface BusDepartureCardProps {
   busNumber: string;
@@ -18,13 +19,21 @@ const BusDepartureCard = ({
   busNumber,
   nextTimestamp,
 }: BusDepartureCardProps) => {
+  // busNumber를 바로 렌더링하면 서버와 클라이언트간 렌더링 트리에 차이가 발생하는 문제가 생긴다.
+  // 이 문제를 회피하기위해 약간 불필요한 처리를 하긴하지만
+  // 클라이언트 단에서 데이터를 설정하고 렌더링하는 방식을 사용한다.
+  const [displayBusNumber, setDisplayBusNumber] = useState('');
+  useEffect(() => {
+    setDisplayBusNumber(busNumber);
+  }, [busNumber]);
+
   return (
     <Card className="relative w-[300px]">
       <CardHeader className="px-6 pb-0 pt-6">
-        <BusNumber busNumber={busNumber} />
+        <BusNumber busNumber={displayBusNumber} />
         <Avatar className="absolute right-4 top-4">
           <AvatarImage />
-          <AvatarFallback>{busNumber}</AvatarFallback>
+          <AvatarFallback>{displayBusNumber}</AvatarFallback>
         </Avatar>
       </CardHeader>
       <CardDescription className="mt-2 px-6">마을버스</CardDescription>
