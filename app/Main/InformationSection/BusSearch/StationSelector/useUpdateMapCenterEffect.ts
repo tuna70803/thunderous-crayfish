3b4d9d7526a49f73421d05a5b65f905532fb851f
tuna70803 <tuna70803@gmail.com>
2024-07-07 21:37:17 +0900

    feat: 정류소 조회 중심에 점표시 추가
    정류소를 조회할 때 조회 기준 위치에 점표시를 추가한다.
    기준은 사용자의 위치일 수도 있지만 지도를 이동했다면 이동한 위치의 중심이
    조회의 기준이 되어 여기에 점이 표시된다. 이 표시로 사용자가 어디를 기준으로
    버스 정류소를 조회했는지 알 수 있다. 점은 Kakao Map의 Marker를 사용하고
    이미지를 따로 준비한다.

--- app/Main/InformationSection/BusSearch/StationSelector/useUpdateMapCenterEffect.ts
+++ app/Main/InformationSection/BusSearch/StationSelector/useUpdateMapCenterEffect.ts
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import type { LatLng } from '@/types';
 
 /**
  * 맵 중앙 위치 업데이트를 사용한다.
+ * 지정한 위치로 맵의 중앙 위치를 이동시키고,
+ * 이 위치에 현위치 마커를 표시한다.
  * @param location - 맵의 중앙으로 업데이트할 위치
  * @param map - Map 인스턴스
  */
@@ -22,6 +24,43 @@ const useUpdateMapCenterEffect = (
     const newCenter = new window.kakao.maps.LatLng(location.lat, location.lng);
     map.panTo(newCenter);
   }, [location]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const [centerMarker, setCenterMarker] = useState<kakao.maps.Marker | null>(
+    null,
+  );
+
+  useEffect(() => {
+    if (!map || !location) {
+      centerMarker?.setMap(null);
+      return;
+    }
+
+    if (!centerMarker) {
+      const markerImageSrc = '/images/user-position.svg';
+      const markerImageSize = new window.kakao.maps.Size(16, 16);
+      const markerImageOffset = new window.kakao.maps.Point(8, 8);
+      const markerImage = new window.kakao.maps.MarkerImage(
+        markerImageSrc,
+        markerImageSize,
+        { offset: markerImageOffset },
+      );
+
+      const marker = new window.kakao.maps.Marker({
+        position: new window.kakao.maps.LatLng(location.lat, location.lng),
+        image: markerImage,
+        title: '내 위치',
+      });
+
+      marker.setMap(map);
+      setCenterMarker(marker);
+
+      return;
+    }
+
+    centerMarker.setPosition(
+      new window.kakao.maps.LatLng(location.lat, location.lng),
+    );
+  }, [map, location, centerMarker]);
 };
 
 export default useUpdateMapCenterEffect;
