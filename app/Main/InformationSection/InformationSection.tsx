fc8262846d0c387ff0d7649a523078aee70ff9ea
tuna <tuna70803@gmail.com>
2024-07-28 14:19:15 +0900

    refactor: 버스 출발 정보 섹션 리팩토링
    버스 출발 시간을 표시하는 섹션 컴포넌트를 리팩토링한다.
    컴포넌트는 섹션 역할을 하지만 아직 세부 정보를 가지고 있어 소스가 약간
    복잡하다. 그러므로 버스 출발 부분의 내용들을 별도 컴포넌트로 분리해서
    섹션 컴포넌트는 전체적인 레이아웃 구성과 기본 정보만 가지고 있게 만든다.
    다음 출발 시간 카드와 그 다음 시간 목록은 별도 컨텐츠 컴포넌트를 만든다.
    기존 내용을 섹션 컴포넌트에서 옮긴다. 즐겨찾기 등록, 해제 소스는 다른
    소스와 연결할 필요가 없으므로 별도의 훅으로 만들어서 사용한다.

--- app/Main/InformationSection/InformationSection.tsx
+++ app/Main/InformationSection/InformationSection.tsx
@@ -1,15 +1,10 @@
 'use client';
-import { useCallback, useState } from 'react';
-import BusDepartureCard from '@/components/widget/BusDepartureCard';
-import PastBusTimes from '@/components/widget/PastBusTimes/PastBusTimes';
-import { toDateString, toTimeString } from '@/utils/dateTime';
+import { useState } from 'react';
 import { getSavedBusRoute, saveBusRoute } from '@/utils/storage';
 import type { BusRoute } from '@/types';
+import DepartureInfo from './DepartureInfo';
 import BusSearch from './BusSearch';
 import Favorites from './Favorites';
-import useBusArrivals from './useBusArrivals';
-import useReferenceDate from './useReferenceDate';
-import useFavorites from '@/store/favorites';
 
 /**
  * 버스 정보 섹션 컴포넌트
@@ -30,52 +25,9 @@ const InformationSection = () => {
     saveBusRoute(targetBus);
   };
 
-  const referenceDate = useReferenceDate();
-  const busArrivals = useBusArrivals(currentBusRoute, referenceDate);
-  const currentTimestamp = Date.now();
-  const todayDateString = toDateString(currentTimestamp);
-  const pastBusTimestamps: number[] = busArrivals.map((busTime: string) => {
-    const todayTimestamp = `${todayDateString}T${busTime.slice(11)}`;
-    return new Date(todayTimestamp).getTime();
-  });
-
-  const futureTimestamps = pastBusTimestamps.filter(
-    (timestamp) => timestamp > currentTimestamp,
-  );
-  const nextTargetTimestamp = futureTimestamps[0] ?? null;
-
-  const futureTimes = futureTimestamps.map((timestamp) =>
-    toTimeString(timestamp),
-  );
-
-  const { addFavorite, removeFavorite, hasFavorite } = useFavorites(
-    (state) => state,
-  );
-
-  const onFavoriteChange = useCallback(
-    (targetBus: Partial<BusRoute>, isFavorited: boolean) => {
-      if (isFavorited) {
-        removeFavorite(targetBus as BusRoute);
-        return;
-      }
-
-      addFavorite(targetBus as BusRoute);
-    },
-    [addFavorite, removeFavorite],
-  );
-
   return (
     <div className="flex flex-col items-center overflow-auto p-10">
-      <BusDepartureCard
-        targetBus={currentBusRoute}
-        nextTimestamp={nextTargetTimestamp}
-        referenceDate={referenceDate}
-        isFavorited={hasFavorite(currentBusRoute)}
-        onFavorite={onFavoriteChange}
-      />
-      <div className="mt-6">
-        <PastBusTimes arrivals={futureTimes} />
-      </div>
+      <DepartureInfo targetBusRoute={currentBusRoute} />
       <BusSearch buttonClass="mt-6" onSearch={onBusRouteChange} />
       <Favorites className="mt-20" onSelect={onBusRouteChange} />
     </div>
